41b0074c4f570fbd348afbd3167d1862e8c84e2e
Dimaboxx <dimaboxx@yandex.ru>
2021-10-18 21:42:12 +0300

    Add project files.

---
+++ BaseWork/Manager.cs
@@ -0,0 +1,27 @@
+﻿using System;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+
+namespace BaseWork
+{
+    public class Manager
+    {
+        public static void GetD(string Path)
+        {
+
+            foreach (var d in Directory.EnumerateDirectories(Path)  )
+            {
+                Console.WriteLine(d.Replace(Path,  new String( ' ' , Path.Length/4)));
+                GetD(d);
+            }
+
+            foreach (var f in Directory.EnumerateFiles(Path))
+            {
+
+                Console.WriteLine(f.Replace(Path,  new String( ' ' , Path.Length/4)+"_"));
+            }
+            ;
+        }
+    }
+}
---
+++ ConsoleClient/Program.cs
@@ -0,0 +1,19 @@
+﻿using System;
+
+namespace ConsoleClient
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            string testdir = @"d:\Export\";
+
+
+            BaseWork.Manager.GetD(testdir);
+
+
+            Console.ReadLine();
+            // Console.WriteLine("Hello World!");
+        }
+    }
+}
